0ce5a6aacd66f6600bba87dfa15e7dc3595dd494
ranjithsai <manikantesairanjith@gmail.com>
2021-08-23 17:47:13 -0700

    fix 1

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const logger = require("morgan");
 const cors = require("cors");
 
 app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(express.static(__dirname));
 
 /** To redirect after auth login */
 app.get('/auth/callback', (req, res) => {
